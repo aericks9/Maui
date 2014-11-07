398a7dc29f33f81586e159827b33deb07a461144
Michael Chary <mac389@gmail.com>
2014-11-07 13:21:31 -0500

    added process-usertimeline PY

---
+++ process-usertimeline.py
@@ -0,0 +1,15 @@
+import json
+
+filename = 'usertimeline.json'
+READ = 'rb'
+tweets = json.load(open(filename,READ))
+
+#Which hashtags were used?
+
+TEXT=1
+hashtags = [word for tweet in tweets for word in tweet['text'][TEXT].split() if '#' in word]
+#print hashtags
+
+#Which tweet is most popular? (Which tweet has the most retweets?)
+#Need retweet field from Twitter
+print tweets
